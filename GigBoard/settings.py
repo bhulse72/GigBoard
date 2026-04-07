5c36bb966c14ff3cd8e097e7b5821c1468989d36
Brandon Hulse <bhulse@mac.dhcp.nd.edu>
2026-04-07 13:56:41 -0400

    added css styling

--- GigBoard/settings.py
+++ GigBoard/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = 'accounts:profile'
 LOGOUT_REDIRECT_URL = 'accounts:login'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
